a53770676a7cc0397fdfe2d47e32e1a815813241
kennankole <kennankole@gmail.com>
2022-11-29 11:09:34 +0300

    Added handlers (handleChange) to the MyCalculator component

--- src/component/Calculator.js
+++ src/component/Calculator.js
@@ -1,52 +1,82 @@
+/* eslint-disable react/no-unused-state */
+/* eslint-disable no-mixed-spaces-and-tabs */
+/* eslint-disable no-tabs */
 import React from 'react';
+import calculate from '../logic/calculate';
 
 // eslint-disable-next-line react/prefer-stateless-function
 class MyCalculator extends React.Component {
-  render() {
-    return (
-      <section>
-        <div className="container">
-          <table>
-            <thead>
-              <tr>
-                <td colSpan="4" className="inputs">0</td>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>AC</td>
-                <td>+</td>
-                <td>%</td>
-                <td className="orange">÷</td>
-              </tr>
-              <tr>
-                <td>7</td>
-                <td>8</td>
-                <td>9</td>
-                <td className="orange">x</td>
-              </tr>
-              <tr>
-                <td>4</td>
-                <td>5</td>
-                <td>6</td>
-                <td className="orange">-</td>
-              </tr>
-              <tr>
-                <td>1</td>
-                <td>2</td>
-                <td>3</td>
-                <td className="orange">+</td>
-              </tr>
-              <tr className="last">
-                <td colSpan="2">0</td>
-                <td>.</td>
-                <td className="orange">=</td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-      </section>
-    );
+  constructor(props) {
+    super(props);
+    this.state = {
+      total: null,
+      operation: null,
+      next: null,
+    };
+    this.handleChange = this.handleChange.bind(this);
   }
+
+	handleChange = (event) => {
+	  const buttonName = event.target.innerHTML;
+	  const results = calculate(this.state, buttonName);
+	  this.setState(results);
+	}
+
+	render() {
+	  const { total, operation, next } = this.state;
+	  return (
+  <section>
+    <div className="container">
+      <table>
+        <thead>
+          <tr>
+            <td
+              colSpan="4"
+              className="inputs"
+              onClick={this.handleChange}
+              role="presentation"
+            >
+              {total}
+              {operation}
+              {next}
+            </td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td onClick={this.handleChange} role="presentation">AC</td>
+            <td onClick={this.handleChange} role="presentation">+</td>
+            <td onClick={this.handleChange} role="presentation">%</td>
+            <td className="orange">÷</td>
+          </tr>
+          <tr>
+            <td onClick={this.handleChange} role="presentation">7</td>
+            <td onClick={this.handleChange} role="presentation">8</td>
+            <td onClick={this.handleChange} role="presentation">9</td>
+            <td className="orange" onClick={this.handleChange} role="presentation">x</td>
+          </tr>
+          <tr>
+            <td onClick={this.handleChange} role="presentation">4</td>
+            <td onClick={this.handleChange} role="presentation">5</td>
+            <td onClick={this.handleChange} role="presentation">6</td>
+            <td onClick={this.handleChange} role="presentation" className="orange">-</td>
+          </tr>
+          <tr>
+            <td onClick={this.handleChange} role="presentation">1</td>
+            <td onClick={this.handleChange} role="presentation">2</td>
+            <td onClick={this.handleChange} role="presentation">3</td>
+            <td onClick={this.handleChange} role="presentation" className="orange">+</td>
+          </tr>
+          <tr className="last">
+            <td onClick={this.handleChange} role="presentation" colSpan="2">0</td>
+            <td onClick={this.handleChange} role="presentation">.</td>
+            <td className="orange" onClick={this.handleChange} role="none">=</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </section>
+	  );
+	}
 }
 export default MyCalculator;
