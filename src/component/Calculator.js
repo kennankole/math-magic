ee981a61b0f4a4a0e97aaae88f7777e11d2b0c86
kennankole <kennankole@gmail.com>
2022-12-01 09:05:57 +0300

    Refactored MyCalculator component into functional component

--- src/component/Calculator.js
+++ src/component/Calculator.js
@@ -1,78 +1,74 @@
-import React from 'react';
+import React, { useState } from 'react';
 import calculate from '../logic/calculate';
 
-class MyCalculator extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      total: null,
-      operation: null,
-      next: null,
-    };
-    this.handleChange = this.handleChange.bind(this);
-  }
+const MyCalculator = () => {
+  const [operation, setOperation] = useState(null);
+  const [total, setTotal] = useState(null);
+  const [next, setNext] = useState(null);
 
-  handleChange = (event) => {
-    const buttonName = event.target.innerHTML;
-    const results = calculate(this.state, buttonName);
-    this.setState(results);
+  const dataObj = { total, operation, next };
+
+  const handleChange = (event) => {
+	  const buttonName = event.target.innerHTML;
+	  const results = calculate(dataObj, buttonName);
+    const { total, operation, next } = results;
+    setOperation(operation);
+  	setNext(next);
+  	setTotal(total);
   };
 
-  render() {
-    const { total, operation, next } = this.state;
-    return (
-      <section>
-        <div className="container">
-          <table>
-            <thead>
-              <tr>
-                <td
-                  colSpan="4"
-                  className="inputs"
-                  onClick={this.handleChange}
-                  role="presentation"
-                >
-                  {total}
-                  {operation}
-                  {next}
-                </td>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td onClick={this.handleChange} role="presentation">AC</td>
-                <td onClick={this.handleChange} role="presentation">+</td>
-                <td onClick={this.handleChange} role="presentation">%</td>
-                <td className="orange" onClick={this.handleChange} role="presentation">÷</td>
-              </tr>
-              <tr>
-                <td onClick={this.handleChange} role="presentation">7</td>
-                <td onClick={this.handleChange} role="presentation">8</td>
-                <td onClick={this.handleChange} role="presentation">9</td>
-                <td className="orange" onClick={this.handleChange} role="presentation">x</td>
-              </tr>
-              <tr>
-                <td onClick={this.handleChange} role="presentation">4</td>
-                <td onClick={this.handleChange} role="presentation">5</td>
-                <td onClick={this.handleChange} role="presentation">6</td>
-                <td onClick={this.handleChange} role="presentation" className="orange">-</td>
-              </tr>
-              <tr>
-                <td onClick={this.handleChange} role="presentation">1</td>
-                <td onClick={this.handleChange} role="presentation">2</td>
-                <td onClick={this.handleChange} role="presentation">3</td>
-                <td onClick={this.handleChange} role="presentation" className="orange">+</td>
-              </tr>
-              <tr className="last">
-                <td onClick={this.handleChange} role="presentation" colSpan="2">0</td>
-                <td onClick={this.handleChange} role="presentation">.</td>
-                <td className="orange" onClick={this.handleChange} role="none">=</td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-      </section>
-    );
-  }
-}
+	  return (
+  <section>
+    <div className="container">
+      <table>
+        <thead>
+          <tr>
+            <td
+              colSpan="4"
+              className="inputs"
+              onClick={handleChange}
+              role="presentation"
+            >
+              {total}
+              {operation}
+              {next}
+            </td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td onClick={handleChange} role="presentation">AC</td>
+            <td onClick={handleChange} role="presentation">+</td>
+            <td onClick={handleChange} role="presentation">%</td>
+            <td className="orange">÷</td>
+          </tr>
+          <tr>
+            <td onClick={handleChange} role="presentation">7</td>
+            <td onClick={handleChange} role="presentation">8</td>
+            <td onClick={handleChange} role="presentation">9</td>
+            <td className="orange" onClick={handleChange} role="presentation">x</td>
+          </tr>
+          <tr>
+            <td onClick={handleChange} role="presentation">4</td>
+            <td onClick={handleChange} role="presentation">5</td>
+            <td onClick={handleChange} role="presentation">6</td>
+            <td onClick={handleChange} role="presentation" className="orange">-</td>
+          </tr>
+          <tr>
+            <td onClick={handleChange} role="presentation">1</td>
+            <td onClick={handleChange} role="presentation">2</td>
+            <td onClick={handleChange} role="presentation">3</td>
+            <td onClick={handleChange} role="presentation" className="orange">+</td>
+          </tr>
+          <tr className="last">
+            <td onClick={handleChange} role="presentation" colSpan="2">0</td>
+            <td onClick={handleChange} role="presentation">.</td>
+            <td className="orange" onClick={handleChange} role="none">=</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </section>
+	  );
+};
 export default MyCalculator;
