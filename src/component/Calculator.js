a118b0c5f534ee6455c00ac21f6bdd12efd8f515
kennankole <kennankole@gmail.com>
2022-12-06 09:08:40 +0300

    Added different pages with Route - Added all the necessary styles for the app.

--- src/component/Calculator.js
+++ src/component/Calculator.js
@@ -21,57 +21,60 @@ class MyCalculator extends React.Component {
   render() {
     const { total, operation, next } = this.state;
     return (
-      <section>
-        <div className="container">
-          <table>
-            <thead>
-              <tr>
-                <td
-                  colSpan="4"
-                  className="inputs"
-                  onClick={this.handleChange}
-                  role="presentation"
-                >
-                  {total}
-                  {operation}
-                  {next}
-                </td>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td onClick={this.handleChange} role="presentation">AC</td>
-                <td onClick={this.handleChange} role="presentation">+</td>
-                <td onClick={this.handleChange} role="presentation">%</td>
-                <td className="orange" onClick={this.handleChange} role="presentation">÷</td>
-              </tr>
-              <tr>
-                <td onClick={this.handleChange} role="presentation">7</td>
-                <td onClick={this.handleChange} role="presentation">8</td>
-                <td onClick={this.handleChange} role="presentation">9</td>
-                <td className="orange" onClick={this.handleChange} role="presentation">x</td>
-              </tr>
-              <tr>
-                <td onClick={this.handleChange} role="presentation">4</td>
-                <td onClick={this.handleChange} role="presentation">5</td>
-                <td onClick={this.handleChange} role="presentation">6</td>
-                <td onClick={this.handleChange} role="presentation" className="orange">-</td>
-              </tr>
-              <tr>
-                <td onClick={this.handleChange} role="presentation">1</td>
-                <td onClick={this.handleChange} role="presentation">2</td>
-                <td onClick={this.handleChange} role="presentation">3</td>
-                <td onClick={this.handleChange} role="presentation" className="orange">+</td>
-              </tr>
-              <tr className="last">
-                <td onClick={this.handleChange} role="presentation" colSpan="2">0</td>
-                <td onClick={this.handleChange} role="presentation">.</td>
-                <td className="orange" onClick={this.handleChange} role="none">=</td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-      </section>
+      <>
+        <h1 className="home-text">Let us do some maths</h1>
+        <section className="calculatorSection">
+          <div className="container">
+            <table>
+              <thead>
+                <tr>
+                  <td
+                    colSpan="4"
+                    className="inputs"
+                    onClick={this.handleChange}
+                    role="presentation"
+                  >
+                    {total}
+                    {operation}
+                    {next}
+                  </td>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td onClick={this.handleChange} role="presentation">AC</td>
+                  <td onClick={this.handleChange} role="presentation">+</td>
+                  <td onClick={this.handleChange} role="presentation">%</td>
+                  <td className="orange" onClick={this.handleChange} role="presentation">÷</td>
+                </tr>
+                <tr>
+                  <td onClick={this.handleChange} role="presentation">7</td>
+                  <td onClick={this.handleChange} role="presentation">8</td>
+                  <td onClick={this.handleChange} role="presentation">9</td>
+                  <td className="orange" onClick={this.handleChange} role="presentation">x</td>
+                </tr>
+                <tr>
+                  <td onClick={this.handleChange} role="presentation">4</td>
+                  <td onClick={this.handleChange} role="presentation">5</td>
+                  <td onClick={this.handleChange} role="presentation">6</td>
+                  <td onClick={this.handleChange} role="presentation" className="orange">-</td>
+                </tr>
+                <tr>
+                  <td onClick={this.handleChange} role="presentation">1</td>
+                  <td onClick={this.handleChange} role="presentation">2</td>
+                  <td onClick={this.handleChange} role="presentation">3</td>
+                  <td onClick={this.handleChange} role="presentation" className="orange">+</td>
+                </tr>
+                <tr className="last">
+                  <td onClick={this.handleChange} role="presentation" colSpan="2">0</td>
+                  <td onClick={this.handleChange} role="presentation">.</td>
+                  <td className="orange" onClick={this.handleChange} role="none">=</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </section>
+      </>
     );
   }
 }
