bfb371928ac51e37cc8693c256ae3a778de8da0d
kennankole <kennankole@gmail.com>
2022-12-06 09:16:34 +0300

    Modified Quote.js file

--- src/component/Quote.js
+++ src/component/Quote.js
@@ -5,7 +5,7 @@ const Quote = () => (
     <p className="text-description">
       The standard chunk of Lorem Ipsum used since the 1500s is reproduced below
       for those interested. Sections 1.10.32 and 1.10.33 from de Finibus Bonorum et Malorum
-      by Cicero are also reproduced in their exact original form, accompanied by
+      by Cicero are also reproduced in their exact original form accompanied by
       English versions from the 1914 translation
       <strong className="author"> by H. Rackham.</strong>
     </p>
